89d064e8fb3252ce74733b5ab63f2a3bf66ec4cc
Magesh Kumar M <Magesh Kumar M@DESKTOP-TP916MO>
2019-06-15 18:38:50 +0530

    fixed error

--- OrderManger/src/main/java/com/online/smartsilk/shop/service/ProductService.java
+++ OrderManger/src/main/java/com/online/smartsilk/shop/service/ProductService.java
@@ -1,12 +1,15 @@
 package com.online.smartsilk.shop.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.assertj.core.util.Arrays;
+
 public class ProductService {
-	
-	public List<ProductDetail> getProductList()
-	{
-		
+
+	public List<String> getProductList() {
+		// To Do
+		return new ArrayList<String>();
 	}
 
 }
